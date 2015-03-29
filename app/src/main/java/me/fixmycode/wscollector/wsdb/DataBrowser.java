75d6a235c9b4df825fb0df9465a7ef73bc2bd154
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2015-03-29 20:35:47 -0300

    Force the upgrade of the database on reinstall.

--- app/src/main/java/me/fixmycode/wscollector/wsdb/DataBrowser.java
+++ app/src/main/java/me/fixmycode/wscollector/wsdb/DataBrowser.java
@@ -19,6 +19,7 @@ public class DataBrowser extends SQLiteAssetHelper {
 
     private DataBrowser(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        setForcedUpgrade();
     }
 
     public static DataBrowser getInstance(Context context){

c27f7bc588de1d28ed23304520053dc67e45eb2b
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2015-04-03 15:01:28 -0300

    fixes SearchView behavior

--- app/src/main/java/me/fixmycode/wscollector/SearchActivity.java
+++ app/src/main/java/me/fixmycode/wscollector/SearchActivity.java
@@ -11,15 +11,20 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import me.fixmycode.wscollector.fragments.CardFragment;
 import me.fixmycode.wscollector.fragments.SearchFragment;
+import me.fixmycode.wscollector.wsdb.Card;
 
 
 public class SearchActivity extends BaseActivity
         implements SearchView.OnQueryTextListener, CardFragment.CardListener,
         FragmentManager.OnBackStackChangedListener {
     public static final String TAG = "ACT_SEARCH";
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,7 @@ public class SearchActivity extends BaseActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, SearchFragment.newInstance(null), TAG)
+                    .replace(R.id.container, SearchFragment.newInstance(null), TAG)
                     .commit();
             if (getIntent() != null) {
                 handleIntent(getIntent());
@@ -41,16 +46,14 @@ public class SearchActivity extends BaseActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search, menu);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView;
-        if (Build.VERSION.SDK_INT > 10) {
-            searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        } else {
-            searchView = (SearchView) MenuItemCompat
-                    .getActionView(menu.findItem(R.id.action_search));
-        }
+        searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false);
         searchView.setOnQueryTextListener(this);
+        if(searchView.requestFocus()){
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.showSoftInput(searchView, InputMethodManager.SHOW_IMPLICIT);
+        }
         return true;
     }
 
@@ -72,6 +75,10 @@ public class SearchActivity extends BaseActivity
 
     @Override
     public boolean onQueryTextSubmit(String s) {
+        FragmentManager manager = getSupportFragmentManager();
+        if(manager.findFragmentByTag("CARD") != null){
+            onSupportNavigateUp();
+        }
         showResults(s);
         return true;
     }
@@ -101,5 +108,13 @@ public class SearchActivity extends BaseActivity
             return false;
         }
         return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void OnCardDisplayed(Card card) {
+        super.OnCardDisplayed(card);
+        if(searchView != null){
+            searchView.clearFocus();
+        }
     }
 }
